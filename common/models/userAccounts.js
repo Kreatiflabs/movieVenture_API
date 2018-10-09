a70b939b880c54715fe1ef35492c305256ae439a
heriipurnama <herii.sinatra@gmail.com>
2018-10-09 15:20:44 +0700

    new user

--- common/models/userAccounts.js
+++ common/models/userAccounts.js
@@ -1,5 +1,32 @@
 'use strict';
 
-module.exports = function(userAccounts) {
+module.exports = function(UserAccounts) {
+  UserAccounts.newUser = async function(data) {
+    const datas = {
+      username:data.username,
+      password:data.password,
+      email:data.email
+    };
+    const userAccountCreate = await UserAccounts.create(datas);
+    return userAccountCreate;
+  };
+
+  UserAccounts.remoteMethod('newUser', {
+    http: {path: '/CreateUser/newUser', verb: 'post'},
+    description: 'Create new user',
+    accepts: [
+      {
+        arg: 'New User',
+        type: 'object',
+        required: true,
+        http: {source: 'body'},
+        description: 'Insert New User'
+      },
+      {arg: 'options', type: 'object', http: 'optionsFromRequest'}
+    ],
+    returns: {arg: 'result', type: 'string', root: true}
+  });
+
+
 
 };
